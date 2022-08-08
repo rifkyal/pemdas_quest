1dacc1b5650bf2eb9517f8d23ed91ebef73e2891
rifkyal <110508879+rifkyal@users.noreply.github.com>
2022-08-08 08:51:15 +0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+#belajar output 
+
+print("Hello World")
+print("Nama Saya Rifky")
+print("Umur Saya 15 Tahun")
+print("Saya Tingggal Di Margonda")
+print("Saya Mempunyai 2 Adik")
+print("Hobby Saya Main Bola")
+print("Saya suka main game")
+print("cita-Cita Saya Menjadi Youtuber Gaming")
+print("Saya Sekolah Di SMK Harapan Bangsa")
+print("Saya belajar pemrograman dasar")
---
+++ python2.py
@@ -0,0 +1,13 @@
+# Belajar Input
+
+nama= "Rifky" # rifky adalah Data, nama adalah 
+umur = 15
+alamat ="margonda, kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah" + nama + " umur saya " + str (umur) + " alamat saya " + alamat )
+
+print("nama saya adalah "+ nama)
